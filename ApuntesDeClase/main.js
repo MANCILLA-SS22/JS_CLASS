187bff7f1426c88a8a8f5e48b0a8d2b3cd85e937
MANCILLA-SS22 <xxel.tiradorxx@gmail.com>
2023-11-16 00:34:25 -0800

    clase 9

--- ApuntesDeClase/main.js
+++ ApuntesDeClase/main.js
@@ -6591,24 +6591,26 @@ app.listen(PORT, () => console.log(`Server listening on port ${PORT}`)); */
 //Ejemplo 21: Express Multer
 import express from "express";
 import {loader} from "./src/utils/multer.js"
-
-const PORT = 5500;
 const app = express();
+const PORT = 5500;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("./src/public/"));
+app.use(express.static("src/public/"));
 
 app.post("/", loader.single("file"), function(request, response){
     if(!request.file){
         return response.status(500).json({
             error: "Hubo un error al subir el archivo"
         });
-    };
+    }else{
+        return response.json({
+            message: "El archivo se subio correctamente"
+        });
+    }
 
-    request.json({
-        message: "El archivo se subio correctamente"
-    })
+    
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
--- ApuntesDeClase/src/utils/multer.js
+++ ApuntesDeClase/src/utils/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function(request, file, cb){
-        cb(null, "../public")
+        cb(null, "src/public")
     },
     filename: function(request, file, cb){
         cb(null, file.originalname)
